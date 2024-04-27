e13eed5dd296913428c9137db24cf66d7cfcccb3
Yash Liladhar Amarapurkar <73273674+Yash19-Yash19@users.noreply.github.com>
2024-04-27 21:34:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-
-
-
-
-# importing libraries
 import streamlit as st #web app and camera
 import numpy as np # for image processing 
 from PIL import Image #Image processing 
@@ -10,8 +5,6 @@ import cv2 #computer vision
 import os
 import time
 
-
-# image conversion functions
 def dodgeV2(x, y):
     return cv2.divide(x, 255 - y, scale=256)
 
@@ -23,204 +16,80 @@ def pencilsketch(inp_img):
     return(final_img)
 
 
-# Use wide option to view full page
-st.set_page_config(layout="wide")
+st.title("PencilSketcher App - updated streamlit camera 📷 module")
+st.write("This Web App is to help convert your photos to realistic Pencil Sketches")
+
+# collect the user input 
+
+# file_image = st.sidebar.file_uploader("Upload your Photos", type=['jpeg','jpg','png'])
+
+# collecting the input image from user camera 
+
+file_image = st.camera_input(label = "Take a pic of you to be sketched out")
+
+uploaded_file = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+
+# Check if a file is uploaded
+if uploaded_file is not None:
+    # Read the contents of the file
+    # image = uploaded_file.read()
+
+    # convert the uploaded image into the pensil sketch
+
+    uploaded_file= Image.open(uploaded_file)
+    final_sketch = pencilsketch(np.array(uploaded_file))
+
+    one, two = st.columns(2)
+    with one:
+        st.write("**Uploaded Photo**")
+        st.image(uploaded_file, use_column_width=True)
+    with two:
+        st.write("**Output Pencil Sketch**")
+        st.image(final_sketch, use_column_width=True)
+
+    # Display the uploaded image
+    # st.image(image, caption='Uploaded Image', use_column_width=True)
 
 
-# setting up app title 
-st.markdown(""" 
-# P2S
-""")
+if file_image:
+    input_img = Image.open(file_image)
+    final_sketch = pencilsketch(np.array(input_img))
+    one, two = st.columns(2)
+    with one:
+        st.write("**Input Photo**")
+        st.image(input_img, use_column_width=True)
+    with two:
+        st.write("**Output Pencil Sketch**")
+        st.image(final_sketch, use_column_width=True)
+    if st.button("Generate image to download...........",):
+        im_pil = Image.fromarray(final_sketch)
+        im_pil.save('final_image.png')
+        with open("final_image.png", "rb") as file:
+            btn = st.download_button(
+            label="Download image",
+            data=file,
+            file_name="final.png",
+            mime="image/png"
+            )
+        # st.button("Download Sketch Images")
+        # st.write('Download completed')
 
 
-# setting up menu  buttons
-rad =st.sidebar.radio(" Menu",["Home","Help","About us","Contact us"])
-
-
-# if else staements for switching from one option to another
-if rad=="Home":
-
-    # displaying an image and tagline
-    # st.markdown("""
-    # #  Image  to Pencil Sketch 
-
-
-
-    # Before you convert  you have to upload or capture an image
-    # for the conversion of image to a pencil sketch image......😊🙂🙂😊😊
-    # """)
-    st.markdown("""""")
-
-# adding ballons animation
-    # st.balloons()
-
-
-    # connecting download features to sidebar functions
-    with st.sidebar:
-            st.title("Image conversion")
-            textOutput = st.selectbox(
-                "How do you want your output data?",
-                ('Select an option','Capture Image', 'Upload Image'))
-                
-                            
-
-
-
-# code for image conversion
-
-
-
-
-
-    if textOutput=='Select an option':
-        displaying an image and tagline
-        st.markdown("""
-        ##  PencilSketch Editor....
-    
-
-        Before you convert  you have to upload or capture an image
-        for the conversion of image to a pensil sketch image......😊🙂🙂😊😊
-        """)
-
-
-    elif textOutput=='Capture Image':
-        file_image = st.camera_input(label = "Take a pic of you to be sketched out")
-
-        if file_image:
-            input_img = Image.open(file_image)
-            final_sketch = pencilsketch(np.array(input_img))
-            one, two = st.columns(2)
-            with one:
-                st.write("**Input Photo**")
-                st.image(input_img, use_column_width=True)
-            with two:
-                st.write("**Output Pencil Sketch**")
-                st.image(final_sketch, use_column_width=True)
-                if st.button("Generate image to download...........",):
-                            im_pil = Image.fromarray(final_sketch)
-                            im_pil.save('final_image.png')
-                            with open("final_image.png", "rb") as file:
-                                btn = st.download_button(
-                                    label="Download image",
-                                    data=file,
-                                    file_name="final.png",
-                                    mime="image/png"
-                            )
-
-                            time.sleep(60)
-                            os.remove(file_path)
-                            file_path ="/PSEditor/final_image.png"
-
-   
-
-
-
-    elif textOutput=='Upload Image':
 
     
-            uploaded_file = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
-    
-
-            if uploaded_file  is not None:
-
-            
-
-
-                    input_img = Image.open(uploaded_file)
-                    final_sketch = pencilsketch(np.array(input_img))
-                    one, two = st.columns(2)
-                    with one:
-                            st.write("**Input Photo**")
-                            st.image(input_img, use_column_width=True)
-                    with two:
-                            st.write("**Output Pencil Sketch**")
-                            st.image(final_sketch, use_column_width=True)
-                    if st.button("Generate image to download...........",):
-                            im_pil = Image.fromarray(final_sketch)
-                            im_pil.save('final_image.png')
-                            with open("final_image.png", "rb") as file:
-                                    btn = st.download_button(
-                                    label="Download image",
-                                    data=file,
-                                    file_name="final.png",
-                                    mime="image/png"
-                                    )
-                            # st.button("Download Sketch Images")
-                            # st.write('Download completed')
-
-                                    time.sleep(60)
-                                    os.remove(file_path)
-                                    file_path ="/PSEditor/final_image.png"       
-
-
-# help section stats here
-if rad=="Help":
-        #  how to use section
-    st.markdown("""
-    ## How to use...
-        """)
-
-
-    st.markdown("""    
-        1.Select an option from the sidebar.
-        """)
-
-
-    st.markdown(""" 
-        2.For capture image,take a picture and for remmoving option use clear photo.
-        """)
-    
-
-    st.markdown("""    
-        3.For upload image option you have to select and upload the image.
-    """)
-
-
-    st.markdown("""    
-        4.After generating the image you will see a Generate download option.Select it and then you 
-        will be able to download the image.""")
-
-
-# note section
-    st.markdown("""    
-    ## Note:
-    """)
-
-
-    st.markdown(""" 
-            1.Do not upload file size greater than 200mb.
-            """)
-
-
-    st.markdown(""" 
-            2.Use appropriate file format like png,jpg & jpeg.
-            """)
-        
-
-    st.markdown(""" 
-            3.App supports only English language.
-            """)
-
-
-    # st.write("Hiii")
-    st.write("This part is still in beta condition.......................👷‍♂️👷‍♂️👷")
-
-
-# About section
-if rad=="About us":
-        
-    st.markdown("""
-    #  Developed by..... me.....
-    """)
-    
-
-
-
-# Contact us section
-if rad=="Contact us":
-    st.write("Courtesy: itsyou  - [Sketch Code](https://github.com/Yash19-Yash19/)")
-    st.markdown("![](https://avatars.githubusercontent.com/u/73273674?s=400&u=2eedf54ee0e7d1bfd8eafc4a78f574074ace7130&v=4)")
+            file_path ="/PSEditor/final_image.png"
 
 
 
 
+        time.sleep(10)
+        os.remove(file_path)
+
+
+else:
+    st.write("You haven't uploaded any image file")
+
+
+st.write("Courtesy: itsyou  - [Sketch Code](https://github.com/Yash19-Yash19/PSEditor)")
+
+st.markdown("![](https://avatars.githubusercontent.com/u/73273674?s=400&u=2eedf54ee0e7d1bfd8eafc4a78f574074ace7130&v=4)")
